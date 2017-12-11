001efe7a0434f70c74a858290da5245e582dd40d
Polo Duf <paulhenri.dufourcq@gmail.com>
2017-12-11 19:53:50 +0100

    Add the show relative and create the logo

--- app/helpers/relatives_helper.rb
+++ app/helpers/relatives_helper.rb
@@ -2,4 +2,8 @@ module RelativesHelper
   def relative_avatar_class(relative)
     relative.mother_side ? "relatives-border-mother-side" : "relatives-border-father-side"
   end
+
+  def age_count_for(relative)
+    ((Date.today - relative.user.date_of_birth)/365).to_i
+  end
 end
