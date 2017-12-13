865e8addfefcc0d64f957761de57b224292fb624
Ronan Girault <girault.ronan@gmail.com>
2017-12-13 21:55:37 +0100

    try to update user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,17 +3,32 @@ class ProfilesController < ApplicationController
   skip_after_action :verify_authorized
 
   def show
+    if @user.params.nil?
+      flash[:notice] = "Oups.. Votre profil n'est pas éditer. Merci de l'éditer pour y accéder."
+      redirect_to edit_profile_path
+    end
   end
 
   def edit
+
   end
 
   def update
+    @user.update(user_params)
+    if @user.save
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   private
 
   def set_user
     @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :nickname, :date_of_birth, :place_of_birth, :gender, :car, :favorite_song, :favorite_movie, :job, :photo)
   end
 end
